1d4a98d18fa1487d0a0cc432d6d1c6b4360c9e86
NFelipao <nfelipe.gonzalez.96@gmail.com>
2022-08-31 10:56:43 -0300

    V 0.7

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 $('.dropdown').hover(function(){ 
   $('.dropdown-toggle', this).trigger('click'); 
 })
+
 function toggleMute() {
    var myAudio = document.getElementById('audio-play');
    myAudio.muted = !myAudio.muted;
